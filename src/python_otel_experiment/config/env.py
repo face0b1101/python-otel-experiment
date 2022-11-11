690613dad2dfedc3cb4f17072149f8dd5865d89a
face0b1101 <6190957+face0b1101@users.noreply.github.com>
2022-11-11 12:24:47 +0000

    code

--- src/python_otel_experiment/config/env.py
+++ src/python_otel_experiment/config/env.py
@@ -7,3 +7,11 @@ BASE_DIR = Path(__file__).parent
 
 LOG_LEVEL = config("LOG_LEVEL", cast=str, default="WARNING")
 DEFAULT_TZ = config("TZ", cast=str, default="Europe/London")
+
+APM_AGENT_KEY = config("APM_AGENT_KEY", cast=str)
+
+OTEL_RESOURCE_ATTRIBUTES = config("OTEL_RESOURCE_ATTRIBUTES", cast=str)
+OTEL_EXPORTER_OTLP_ENDPOINT = config("OTEL_EXPORTER_OTLP_ENDPOINT", cast=str)
+OTEL_EXPORTER_OTLP_HEADERS = config("OTEL_EXPORTER_OTLP_HEADERS", cast=str)
+OTEL_METRICS_EXPORTER = config("OTEL_METRICS_EXPORTER", cast=str)
+OTEL_LOGS_EXPORTER = config("OTEL_LOGS_EXPORTER", cast=str)
--- src/python_otel_experiment/main.py
+++ src/python_otel_experiment/main.py
@@ -1,28 +1,105 @@
 #!/usr/bin/env python
 
+# OTEL Python test for exporting basic OpenTelemetry data to Elastic APM
+
+# OTLP reference code shamelessly stolen from:
+# https://opentelemetry.io/docs/instrumentation/python/getting-started/
+# https://opentelemetry.io/docs/instrumentation/python/exporters/#otlp-endpoint-or-collector
+
 import logging
+import urllib.parse
+
+# These are the necessary import declarations
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+from opentelemetry import metrics
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
+from random import randint
+from flask import Flask, request
 
 from rich import print
 from rich.logging import RichHandler
 
-from .config.env import DEFAULT_TZ, LOG_LEVEL
+from .config.env import (
+    DEFAULT_TZ,
+    LOG_LEVEL,
+    APM_AGENT_KEY,
+    OTEL_RESOURCE_ATTRIBUTES,
+    OTEL_EXPORTER_OTLP_ENDPOINT,
+    OTEL_EXPORTER_OTLP_HEADERS,
+    OTEL_METRICS_EXPORTER,
+    OTEL_LOGS_EXPORTER,
+)
+
+# Service name is required for most backends
+resource = Resource(
+    attributes={
+        "service.name": "Python OTEL Experiment",
+        "service.version": 0.1,
+        "deployment.environment": "dev",
+    }
+)
+
+resource = Resource(attributes={SERVICE_NAME: "Python OTEL Experiment"})
+
+# OTLP Tracer configuration
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(
+    OTLPSpanExporter(
+        endpoint=OTEL_EXPORTER_OTLP_ENDPOINT, headers=OTEL_EXPORTER_OTLP_HEADERS
+    )
+)
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+
+# OTLP Metrics configuration
+reader = PeriodicExportingMetricReader(
+    OTLPMetricExporter(
+        endpoint=OTEL_EXPORTER_OTLP_ENDPOINT, headers=OTEL_EXPORTER_OTLP_HEADERS
+    )
+)
+provider = MeterProvider(resource=resource, metric_readers=[reader])
+metrics.set_meter_provider(provider)
+
+# Acquire a tracer
+tracer = trace.get_tracer(__name__)
+
+# Acquire a meter.
+meter = metrics.get_meter(__name__)
+
+# Now create a counter instrument to make measurements with
+roll_counter = meter.create_counter(
+    "roll_counter",
+    description="The number of rolls by roll value",
+)
+
+app = Flask(__name__)
+
+# Check for debug environment variable
+if LOG_LEVEL.upper() == "DEBUG":
+    app.debug = True
 
 
-def something(input: str) -> bool:
-    """
-    Simple function that returns True if the input is a string.
-    For test purposes only.
+@app.route("/rolldice")
+def roll_dice():
+    return str(do_roll())
 
-    Args:
-        input (str): Any input string
 
-    Returns:
-        bool: Boolean indicating whether the function was successful or not
-    """
-    if type(input) == str:
-        return True
-    else:
-        return False
+def do_roll():
+    with tracer.start_as_current_span("do_roll") as rollspan:
+        res = randint(1, 6)
+        rollspan.set_attribute("roll.value", res)
+        # This adds 1 to the counter for the given roll value
+        roll_counter.add(1, {"roll.value": res})
+        return res
 
 
 def main():
@@ -39,10 +116,7 @@ def main():
 
     logging.info(f"LOG_LEVEL: {logging.getLevelName(logging.root.level)}")
     logging.info(f"DEFAULT_TZ: {DEFAULT_TZ}")
+    logging.debug(f"ENDPOINT: {OTEL_EXPORTER_OTLP_ENDPOINT}")
+    logging.debug(f"HEADER: {OTEL_EXPORTER_OTLP_HEADERS}")
 
-    input = "Hello world!"
-
-    if something(input):
-        print(input)
-    else:
-        print("Nope!")
+    app.run()
